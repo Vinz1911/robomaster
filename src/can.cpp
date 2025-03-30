d7da1f8b1f9af8fa6ba00f9673c8c354fff95a29
Vinzenz Weist <Vinz1911@gmail.com>
2025-03-30 15:47:22 +0200

    [M] minor changes

--- src/can.cpp
+++ src/can.cpp
@@ -34,8 +34,8 @@
 
 namespace robomaster {
     CANBus::CANBus(): socket_{}, interface_{}, address_{} {
-        memset(&this->interface_, 0x0, sizeof(this->interface_));
-        memset(&this->address_, 0x0, sizeof(this->address_));
+        std::memset(&this->interface_, 0x0, sizeof(this->interface_));
+        std::memset(&this->address_, 0x0, sizeof(this->address_));
     }
 
     CANBus::~CANBus() {
@@ -65,14 +65,14 @@ namespace robomaster {
 
     bool CANBus::send_frame(const uint32_t device_id, const uint8_t data[8], const size_t length) const {
         if (length > 8) { std::printf("[Robomaster]: failed to send can frame\n"); return false; }
-        can_frame frame{}; memset(&frame, 0x0, sizeof(frame));
+        can_frame frame{}; std::memset(&frame, 0x0, sizeof(frame));
         frame.can_id = static_cast<int>(device_id); frame.can_dlc = length; std::memcpy(frame.data, data, length);
         if (write(this->socket_, &frame, sizeof(frame)) < 0x0) { std::printf("[Robomaster]: failed to send can frame\n"); return false; }
         return true;
     }
 
     bool CANBus::read_frame(uint32_t& device_id, uint8_t data[8], size_t& length) const {
-        can_frame frame{}; memset(&frame, 0x0, sizeof(frame));
+        can_frame frame{}; std::memset(&frame, 0x0, sizeof(frame));
         if(read(this->socket_, &frame, sizeof(frame)) < 0x0) { std::printf("[Robomaster]: failed to read can frame\n"); return false; }
         device_id = frame.can_id & CAN_EFF_FLAG ? frame.can_id & CAN_EFF_MASK: frame.can_id & CAN_SFF_MASK;
         length = frame.can_dlc; std::memcpy(data, frame.data, length);
